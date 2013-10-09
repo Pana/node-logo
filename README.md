44b5c65ad2e7d348fac5126e6f7ec0c961cf0145
pana <pana.wang@outlook.com>
2013-10-09 21:20:24 +0800

    first version

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-html hexagon
+html nodejs logo
 =======
 
 A small js lib draw hexagon use html
@@ -12,9 +12,17 @@ A small js lib draw hexagon use html
 * 使用简单
 * 兼容多浏览器, 多版本
 * 考虑添加3d
+* 不依赖第三方库
 
 
+## Html版本原理
+使用三个长方形, 其中两个使用css3的旋转特性分别进行正负60度旋转. 三个长方形的中心必须相同.
+长宽满足: width = height * Math.sqrt(3)
 
-## todo
 
-* 边长关系计算
+## 实现方式
+
+* html version
+* canvas version
+* svg version
+* 3D version
--- hexa.js
+++ hexa.js
@@ -1,19 +1,91 @@
 /*
-*   name: Hexagon
-*   version: 0.0.1
-*   author: Pana
-*   date: 2013-10-09
+*   Name: NodeLogo
+*   Version: 0.0.1
+*   Author: Pana
+*   Date: 2013-10-09
 */
 (function(){
 
-    /*
-        options
-        container:
-        width:
-        backcolor:
-    */
-    function Hexo(options){
+    VERSION = '0.0.1';
 
+    /*
+        常量
+    */ 
+    var RATIO = Math.sqrt(3);
+
+    /*
+        html元素create helper
+    */
+    function cDom(tagName){
+        return document.createElement(tagName);
     }
 
-}).call(this)
+    /*
+        id生成器
+    */
+    function idGenerator(){
+        return 'hexo'+ Date.now();
+    }
+
+    /*
+        样式
+    */
+    function hexaStyle(id, width, color){
+        var dom = cDom('style')
+            , idS = '#' + id
+            , height = width
+            , paddingTop = parseInt(width/2)
+            , lWidth = parseInt(width * RATIO)
+            , containerHei = width + paddingTop
+        var style = idS + '{width:' + lWidth + 'px;padding-top:' + paddingTop + 'px;height: ' + containerHei + 'px;}';
+        style += idS + ' .hex{width:' + lWidth + 'px;height:' + height + 'px;background-color:' + color + '}';
+        dom.innerHTML = style;
+        return dom;
+    }
+
+    /*
+        options
+        container: id
+        width:
+        backcolor: 
+    */
+    function NodeLogo(options){
+        var container = document.getElementById(options.container);
+        if(!container){
+            throw new Error('Container were not found');
+            return;
+        }
+        var hex = cDom('div')
+            , hex1 = cDom('div')
+            , hex2 = cDom('div')
+            , hex3 = cDom('div')
+            , id = idGenerator();
+
+        hex.id = id;
+        hex1.className = 'hex hex1';
+        hex2.className = 'hex hex2';
+        hex3.className = 'hex hex3';
+        hex.appendChild(hex1);
+        hex.appendChild(hex2);
+        hex.appendChild(hex3);
+        var styleDom = hexaStyle(id, options.width, options.backcolor);
+        hex.appendChild(styleDom);
+        container.appendChild(hex);
+    }
+
+    /*
+        添加 common style
+    */
+    (function initStyle(){
+        var styleDom = cDom('style')
+            , style = '.hex{position: absolute;}'+
+            '.hex.hex2{-webkit-transform: rotate(60deg);-moz-transform: rotate(60deg);-ms-transform: rotate(60deg);-o-transform: rotate(60deg);transform: rotate(60deg);}'+
+            '.hex.hex3{-webkit-transform: rotate(-60deg);-moz-transform: rotate(-60deg);-ms-transform: rotate(-60deg);-o-transform: rotate(-60deg);transform: rotate(-60deg);}';
+        styleDom.innerHTML = style;
+        document.head.appendChild(styleDom);
+    })();
+
+    // exports
+    this.NodeLogo = NodeLogo;
+
+}).call(window);
