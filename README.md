2ee485a9a6a8389962d412b3564da4ff8fc7721f
pana <pana.wang@outlook.com>
2013-10-09 19:42:21 +0800

    add file

--- README.md
+++ README.md
@@ -2,3 +2,19 @@ html hexagon
 =======
 
 A small js lib draw hexagon use html
+
+
+
+## target
+
+* 使用html&css画hexagon
+* 能够配置边长, 背景颜色, 背景图片
+* 使用简单
+* 兼容多浏览器, 多版本
+* 考虑添加3d
+
+
+
+## todo
+
+* 边长关系计算
---
+++ hexa.js
@@ -0,0 +1,19 @@
+/*
+*   name: Hexagon
+*   version: 0.0.1
+*   author: Pana
+*   date: 2013-10-09
+*/
+(function(){
+
+    /*
+        options
+        container:
+        width:
+        backcolor:
+    */
+    function Hexo(options){
+
+    }
+
+}).call(this)
