885de03d0ed8b1bd73c97f364200e0707373c78e
pana <pana.wang@outlook.com>
2013-10-10 09:13:56 +0800

    add file

---
+++ canvas.js
@@ -0,0 +1,17 @@
+/*
+*   canvas version
+*   author: Pana
+*/
+
+(function(){
+
+    /*
+        根据边的宽度计算六个点的坐标
+    */
+    function getPoints(){
+        // 通过width计算出canvas的宽度和长度
+        // 从最高点算起
+    }
+
+
+}).call(window)
